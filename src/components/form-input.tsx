24de038b717b9c22deafd298e19b93683240e154
Emre Ozturk <emreeozturk42@gmail.com>
2024-08-04 02:30:21 +0300

    chore: Add FormInput component for handling form input

---
+++ src/components/form-input.tsx
@@ -0,0 +1,31 @@
+import { motion } from 'framer-motion'
+import { useFormStatus } from 'react-dom'
+const FormInput = ({
+    border,
+    boxShadow,
+}: { border: any, boxShadow: any }
+
+) => {
+    const { pending } = useFormStatus()
+    return (
+        <motion.input
+            style={{
+                border,
+                boxShadow,
+            }}
+            disabled={pending}
+            whileHover={{
+                scale: 1.015,
+            }}
+            whileTap={{
+                scale: 0.985,
+            }}
+            type="text"
+            name="address"
+            placeholder="Enter your address"
+            className="group relative flex w-96 mt-12 items-center gap-1.5 rounded-full bg-gray-950/10 px-4 py-2 text-gray-50 transition-colors hover:bg-gray-950/50"
+        />
+    )
+}
+
+export default FormInput
