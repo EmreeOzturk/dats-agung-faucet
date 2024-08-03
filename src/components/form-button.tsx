d497b5155485b3d4d6097b10c220f7b204dd2d41
Emre Ozturk <emreeozturk42@gmail.com>
2024-08-04 02:30:07 +0300

    feat: Add FormButton component for submitting forms

---
+++ src/components/form-button.tsx
@@ -0,0 +1,28 @@
+import { motion } from 'framer-motion'
+import { useFormStatus } from 'react-dom'
+import { LoaderCircle } from 'lucide-react'
+const FormButton = ({ border }: { border: any }) => {
+    const { pending } = useFormStatus()
+    return (
+        <motion.button
+            disabled={pending}
+            type="submit"
+            style={{
+                border,
+            }}
+            whileHover={{
+                scale: 1.015,
+            }}
+            whileTap={{
+                scale: 0.985,
+            }}
+            className="mt-6 px-4 py-2 rounded-full text-zinc-300 font-bold tracking-wider bg-transparent "
+        >
+            {pending ?
+                <LoaderCircle size={20} className='text-sky-500 animate-spin' />
+                : "Claim AGUNG Tokens 🚀"}
+        </motion.button>
+    )
+}
+
+export default FormButton
