f5a906e17033989f754d841846e52b0c39df9b17
Emre Ozturk <emreeozturk42@gmail.com>
2024-11-27 19:01:09 +0300

    feat: Add rewrites for Umami tracking and include script in layout

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/umami.js",
+        destination: "http://3.90.229.127:3000/script.js",
+      },
+      {
+        source: "/api/send",
+        destination: "http://3.90.229.127:3000/api/send",
+      },
+    ];
+  },
+};
 
 export default nextConfig;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Actor } from "next/font/google";
 import "./globals.css";
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import Script from "next/script";
 const inter = Actor({ weight: ["400"], subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -16,6 +17,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          defer
+          src="/umami.js"
+          data-website-id="0d77b307-3e60-4821-af5d-4edc50bc936e"
+        />
+      </head>
       <body className={`${inter.className} select-none overflow-hidden`}>{children}
         <SpeedInsights />
       </body>
