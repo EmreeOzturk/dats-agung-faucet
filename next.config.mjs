5f280da82f0c0ef129e2b47975a27b53d2280f58
Emre Ozturk <emreeozturk42@gmail.com>
2024-12-03 17:32:56 +0300

    chore: remove umami

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/umami.js",
-        destination: "http://3.90.229.127:3000/script.js",
-      },
-      {
-        source: "/api/send",
-        destination: "http://3.90.229.127:3000/api/send",
-      },
-    ];
-  },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: "/umami.js",
+  //       destination: "http://3.90.229.127:3000/script.js",
+  //     },
+  //     {
+  //       source: "/api/send",
+  //       destination: "http://3.90.229.127:3000/api/send",
+  //     },
+  //   ];
+  // },
 };
 
 export default nextConfig;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Actor } from "next/font/google";
 import "./globals.css";
 import { SpeedInsights } from "@vercel/speed-insights/next"
-import Script from "next/script";
+// import Script from "next/script";
 const inter = Actor({ weight: ["400"], subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -17,13 +17,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
+      {/* <head>
         <Script
           defer
           src="/umami.js"
           data-website-id="0d77b307-3e60-4821-af5d-4edc50bc936e"
         />
-      </head>
+      </head> */}
       <body className={`${inter.className} select-none overflow-hidden`}>{children}
         <SpeedInsights />
       </body>
